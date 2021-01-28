c24f6418d5e57732f45cd56ed0f91c77fa2a828a
Ccourter11 <courtercalvin@yahoo.com>
2021-01-28 10:28:24 -0600

    about to start rending the saved notes

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,17 +1,19 @@
+import { saveNote } from "./NoteDataProvider.js";
 const contentTarget = document.querySelector(".noteFormContainer");
 
 const render = () => {
   contentTarget.innerHTML = `
     <form action="" method="get" class="noteForm">
     <fieldset>
+    <h3>Make a Note</h3>
         <label for="dateOf">Entry Date</label>
-        <input type="date" name="dateOf" id="dateOf"></input>
+        <input type="date" name="note-date" id="dateOf"></input>
 
         <label for="suspect">Suspect : </label>
-        <input type="text" name="suspect" id="suspect"></input>
+        <input type="text" name="note-suspect" id="suspect"></input>
 
         <label for="noteEntry">Notes</label>
-        <textarea id="noteEntry" name="noteEntry" row="5" cols="13" placeholder="Notes about case..."></textarea>
+        <textarea id="note-text" name="noteEntry" row="5" cols="13" placeholder="Notes about case..."></textarea>
 
     </fieldset>
     <button type="submit" value="submit" id="saveNote">Save Note</button>
@@ -21,3 +23,25 @@ const render = () => {
 export const NoteForm = () => {
   render();
 };
+
+// Handle browser-generated click event in component
+const eventHub = document.querySelector(".container");
+eventHub.addEventListener("click", (clickEvent) => {
+  clickEvent.preventDefault();
+  if (clickEvent.target.id === "saveNote") {
+    // Make a new object representation of a note
+    const newNote = {
+      date: document.getElementById("dateOf").value,
+      suspect: document.getElementById("suspect").value,
+      subject: document.getElementById("note-text").value,
+      note: document.getElementById(""),
+    };
+
+    // Change API state and application state
+    saveNote(newNote);
+  }
+});
+
+// const NoteForm = () => {
+//   // rest of the code here
+// };
