64025e613a1c38ce5ed80ac6b6b98ebb25d9a620
Ccourter11 <courtercalvin@yahoo.com>
2021-02-11 11:54:57 -0600

    done

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,19 @@
 import { ConvictionSelect } from "./convictions/ConvictionSelect.js";
 import { CriminalList } from "./criminals/CriminalList.js";
 import { NoteForm } from "./notes/NoteForm.js";
+getCriminals().then(NoteForm);
 import { ShowWitnessButton } from "./witnesses/ShowWitnessesButton.js";
 import { ShowNoteButton } from "./notes/ShowNotesButton.js";
 import { OfficerList } from "./officers/OfficerList.js";
 import { OfficerSelect } from "./officers/OfficerSelect.js";
 import "./notes/NoteList.js";
+import { getCriminals } from "./criminals/CriminalProvider.js";
 
 OfficerList();
 CriminalList();
 ConvictionSelect();
 OfficerSelect();
+getCriminals();
 NoteForm();
 ShowNoteButton();
 ShowWitnessButton();
