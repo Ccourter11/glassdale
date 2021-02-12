77cb1217ccb77394329c449d404cfb70ac820f37
Ccourter11 <courtercalvin@yahoo.com>
2021-02-12 10:27:18 -0600

    rendering features

--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,42 +1,45 @@
 import { getNotes, useNotes } from "./NoteDataProvider.js";
 import { NoteHTMLConverter } from "./Note.js";
+import { useCriminals, getCriminals } from "../criminals/CriminalProvider.js";
 
-// Query the DOM for the element that your notes will be added to
-const contentTarget = document.querySelector(".notesContainer");
-// Define ye olde Evente Hubbe
-const eventHub = document.querySelector(".container");
-
-const render = (noteArray, criminalArray) => {
-  const allNotesConvertedToStrings = noteArray
-    .map((noteObject) => {
-      return NoteHTMLConverter(noteObject);
-
-      // convert the notes objects to HTML with NoteHTMLConverter
+const render = (noteCollection, criminalCollection) => {
+  const allNotesConvertedToStrings = noteCollection
+    .map((note) => {
+      // Find the related criminal
+      const relatedCriminal = criminalCollection.find(
+        (criminal) => criminal.id === parseInt(note.criminalId)
+      );
+      return NoteHTMLConverter(note, relatedCriminal);
     })
-    .join("");
+    .join(" ");
 
   contentTarget.innerHTML = `
-    <h3>Case Notes</h3>
-    <section class="notesList">
-    ${allNotesConvertedToStrings}
-    </section>
-  `;
+    <h2>Case Notes</h2><section class="noteEntry">${allNotesConvertedToStrings}</section>
+    `;
 };
 
-eventHub.addEventListener("showNotesClicked", (customEvent) => {
-  NoteList();
+export const NoteList = () => {
+  getNotes()
+    .then(getCriminals)
+    .then(() => {
+      const notes = useNotes();
+      const criminals = useCriminals();
+      render(notes, criminals);
+    });
+};
+
+// Query the DOM for the element that your notes will be added to
+const contentTarget = document.querySelector(".noteList");
+// Define ye olde Evente Hubbe
+const eventHub = document.querySelector(".container");
+// debugger
+
+//listens for change in note form
+eventHub.addEventListener("noteStateChanged", (event) => {
+  if (contentTarget.innerHTML === "") NoteList();
 });
 
-// Standard list function you're used to writing by now. BUT, don't call this in main.js! Why not?
-export const NoteList = () => {
-  getNotes().then(() => {
-    const allNotes = useNotes();
-    render(allNotes);
-  });
-};
-
-eventHub.addEventListener("noteStateChanged", (event) => {
-  if (contentTarget.innerHTML !== "") {
-    NoteList();
-  }
+//listens for Show Note clicked
+eventHub.addEventListener("showNotesClicked", (customEvent) => {
+  NoteList();
 });
