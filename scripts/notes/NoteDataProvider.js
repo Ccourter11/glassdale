7add964054551f32fd9cfa9c7d3e5da340a3e322
Ccourter11 <courtercalvin@yahoo.com>
2021-02-16 13:35:19 -0600

    completed ch15

--- scripts/notes/NoteDataProvider.js
+++ scripts/notes/NoteDataProvider.js
@@ -29,6 +29,8 @@ export const saveNote = (note) => {
     .then(dispatchStateChangeEvent);
 };
 
+// delete method
+
 export const deleteNote = (noteId) => {
   return fetch(`http://localhost:8088/notes/${noteId}`, {
     method: "DELETE",
